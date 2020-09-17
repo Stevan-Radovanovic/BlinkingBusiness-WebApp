1a4ea0f736462df6734bb9a6fac3dc538749b3c6
Stevan <stevan.radovanovic@mmklab.org>
2020-09-17 23:12:35 +0200

    Added initial components and guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
+import { LoginComponent } from './login/login.component';
+import { MainFormComponent } from './main-form/main-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainFormComponent
   ],
   imports: [
     BrowserModule,
