2db364fd8c6659420fc4373e804e644b2e75ecc0
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-09-18 17:52:13 +0200

    [Commit #10] Added log in form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  authForm: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  initForm() {
+    this.authForm = new FormGroup({
+      userName: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
 
+  logIn() {
+    localStorage.setItem('token', 'randomValue');
+    this.router.navigateByUrl('/form');
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
 }
