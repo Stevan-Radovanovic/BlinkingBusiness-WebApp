cbfa3efa24ea2e17852253c009693c0fadd55b02
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-09-18 19:27:28 +0200

    [Commit #15] Design form #2

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 export class AuthService {
   constructor(private router: Router) {}
 
-  loggedIn = false;
+  loggedIn = true;
 
   logIn() {
     localStorage.setItem('token', 'randomValue');
