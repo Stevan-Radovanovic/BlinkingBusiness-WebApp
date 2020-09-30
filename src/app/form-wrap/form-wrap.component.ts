a44f6c2f719f8c56da293c62e37a329a49508fd5
Stevan Radovanovic <stevan.radovanovic@mmklab.org>
2020-09-30 11:32:46 +0200

    [Commit #52] Refactoring #2

--- src/app/form-wrap/form-wrap.component.ts
+++ src/app/form-wrap/form-wrap.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
-import { v4 as uuidv4 } from 'uuid';
-import { FormObject } from '../shared/models/form-object.model';
-import { FormSubmitComponent } from './form-submit/form-submit.component';
-import { frontBack } from '../shared/validators/front-back.validator';
 
 @Component({
   selector: 'app-form-wrap',
@@ -12,59 +6,7 @@ import { frontBack } from '../shared/validators/front-back.validator';
   styleUrls: ['./form-wrap.component.css'],
 })
 export class FormWrapComponent implements OnInit {
-  sessionForm: FormGroup;
+  constructor() {}
 
-  constructor(public dialog: MatDialog) {}
-
-  initSessionForm() {
-    this.sessionForm = new FormGroup({
-      baseRedirectUrl: new FormControl('', [Validators.required]),
-      blinkingParams: new FormControl([], [Validators.required]),
-      willEmbedInIframe: new FormControl(false),
-      skippableSteps: new FormControl([]),
-      stepsThatRequireProofOfDocuments: new FormControl([]),
-      initialSessionConfig: new FormControl('', [
-        Validators.required,
-        frontBack,
-      ]),
-      stepsThatRequireAttention: new FormControl([]),
-    });
-  }
-
-  submit() {
-    const formObject: FormObject = {
-      baseRedirectUrl: this.sessionForm.controls.baseRedirectUrl.value,
-      blinkingParams: this.sessionForm.controls.blinkingParams.value,
-      willEmbedInIframe: this.sessionForm.controls.willEmbedInIframe.value,
-      serviceConfiguration: {
-        skippableSteps: this.sessionForm.controls.skippableSteps.value,
-        stepsThatRequireAttention: this.sessionForm.controls
-          .stepsThatRequireAttention.value,
-        stepsThatRequireProofOfDocuments: this.sessionForm.controls
-          .stepsThatRequireProofOfDocuments.value,
-        initialSessionConfig: this.sessionForm.controls.initialSessionConfig
-          .value,
-      },
-    };
-
-    const dialogRef = this.dialog.open(FormSubmitComponent, {
-      width: '500px',
-      data: { formObject, id: uuidv4() },
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      this.sessionForm.reset({
-        willEmbedInIframe: false,
-        skippableSteps: [],
-        stepsThatRequireProofOfDocuments: [],
-        stepsThatRequireAttention: [],
-      });
-    });
-
-    console.log(formObject);
-  }
-
-  ngOnInit(): void {
-    this.initSessionForm();
-  }
+  ngOnInit(): void {}
 }
--- src/app/form-wrap/session-form/session-form.component.ts
+++ src/app/form-wrap/session-form/session-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { FormObject } from 'src/app/shared/models/form-object.model';
+import { FormSubmitComponent } from '../form-submit/form-submit.component';
+import { v4 as uuidv4 } from 'uuid';
+import { frontBack } from 'src/app/shared/validators/front-back.validator';
 
 @Component({
   selector: 'app-session-form',
@@ -7,9 +12,59 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./session-form.component.css'],
 })
 export class SessionFormComponent implements OnInit {
-  @Input() sessionForm: FormGroup;
+  sessionForm: FormGroup;
 
-  constructor() {}
+  initSessionForm() {
+    this.sessionForm = new FormGroup({
+      baseRedirectUrl: new FormControl('', [Validators.required]),
+      blinkingParams: new FormControl([], [Validators.required]),
+      willEmbedInIframe: new FormControl(false),
+      skippableSteps: new FormControl([]),
+      stepsThatRequireProofOfDocuments: new FormControl([]),
+      initialSessionConfig: new FormControl('', [
+        Validators.required,
+        frontBack,
+      ]),
+      stepsThatRequireAttention: new FormControl([]),
+    });
+  }
 
-  ngOnInit(): void {}
+  submit() {
+    const formObject: FormObject = {
+      baseRedirectUrl: this.sessionForm.controls.baseRedirectUrl.value,
+      blinkingParams: this.sessionForm.controls.blinkingParams.value,
+      willEmbedInIframe: this.sessionForm.controls.willEmbedInIframe.value,
+      serviceConfiguration: {
+        skippableSteps: this.sessionForm.controls.skippableSteps.value,
+        stepsThatRequireAttention: this.sessionForm.controls
+          .stepsThatRequireAttention.value,
+        stepsThatRequireProofOfDocuments: this.sessionForm.controls
+          .stepsThatRequireProofOfDocuments.value,
+        initialSessionConfig: this.sessionForm.controls.initialSessionConfig
+          .value,
+      },
+    };
+
+    const dialogRef = this.dialog.open(FormSubmitComponent, {
+      width: '500px',
+      data: { formObject, id: uuidv4() },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      this.sessionForm.reset({
+        willEmbedInIframe: false,
+        skippableSteps: [],
+        stepsThatRequireProofOfDocuments: [],
+        stepsThatRequireAttention: [],
+      });
+    });
+
+    console.log(formObject);
+  }
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.initSessionForm();
+  }
 }
