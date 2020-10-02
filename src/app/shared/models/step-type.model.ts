78fadaa715fa50aac2e779675dcdab45579a582d
Stevan Radovanovic <stevan.radovanovic@mmklab.org>
2020-10-02 09:40:13 +0200

    [Commit #77] Added sub type and step type

---
+++ src/app/shared/models/step-type.model.ts
@@ -0,0 +1,12 @@
+export enum StepType {
+  COUNTRY = 'Document type with country',
+  DOCUMENT = 'Document type',
+  FRONT = 'Front side',
+  BACK = 'Back side',
+  ADDRESS = 'Address',
+  ACCOUNT = 'Account number',
+  FACE = 'Face',
+  CONTACT = 'Contact data',
+  VIDEO = 'Video session',
+  ADDITIONAL = 'Additional document',
+}
---
+++ src/app/shared/models/sub-type.model.ts
@@ -0,0 +1,6 @@
+export enum SubType {
+  DOCUMENT_COPY = 'Document copy',
+  PROOF_OF_ADDRESS = 'Proof of address',
+  PROOF_OF_INCOME = 'Proof of income',
+  OTHER = 'Other',
+}
