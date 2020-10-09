d8067b4632c51a1e13be024b6a7a19009388a49f
Stevan Radovanovic <stevan.radovanovic@mmklab.org>
2020-10-09 13:36:48 +0200

    [Commit #135] Added two fields in user form

--- src/app/form-wrap/user-wrap/user-form/user-form.component.ts
+++ src/app/form-wrap/user-wrap/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/shared/models/user.model';
 
 @Component({
@@ -9,7 +10,39 @@ import { User } from 'src/app/shared/models/user.model';
 export class UserFormComponent implements OnInit {
   @Input() user: User;
 
+  userForm: FormGroup;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initUserForm();
+  }
+
+  requiredValidator(controlName: string) {
+    return (
+      this.userForm.get(controlName).hasError('required') &&
+      this.userForm.get(controlName).touched
+    );
+  }
+
+  initUserForm() {
+    this.userForm = new FormGroup({
+      name: new FormControl(
+        { value: this.user.name, disabled: true },
+        Validators.required
+      ),
+      roles: new FormControl(
+        { value: this.user.roles, disabled: true },
+        Validators.required
+      ),
+      services: new FormControl(
+        { value: this.user.services, disabled: true },
+        Validators.required
+      ),
+      status: new FormControl(
+        { value: this.user.status, disabled: true },
+        Validators.required
+      ),
+    });
+  }
 }
